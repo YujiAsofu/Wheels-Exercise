3ed996179aced3abf0439b920c6fcd6d13d496ef
Yuji <yujiasofu@gmail.com>
2023-09-03 20:05:49 -0300

    adding wheels

---
+++ wheels.py
@@ -0,0 +1,9 @@
+player_nexus_health = 15
+cpu_nexus_health = 15
+player_barrier_points = 0
+cpu_barrier_points = 0
+player_attack_points = 0
+cpu_attack_points = 0
+
+def cpuAttackFunction():
+    player_nexus_health - cpu_attack_points
